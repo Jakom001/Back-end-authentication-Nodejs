35a4f57165552aa64e0d0e820ac22825447f2344
jakom001 <andrewpeter2032@gmail.com>
2024-10-22 21:19:22 +0300

    create, get single post, update and delete post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,3 +1,4 @@
+const { createPostSchema } = require('../middlewares/validator');
 const Post = require('../models/postsModel');
 
 
@@ -22,6 +23,118 @@ exports.getPosts = async (req, res) => {
 			});
 		res.status(200).json({ success: true, message: 'posts', data: result });
 	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.singlePost = async (req, res) => {
+	const { _id } = req.query;
+
+	try {
+		const existingPost = await Post.findOne({ _id }).populate({
+			path: 'userId',
+			select: 'email',
+		});
+		if (!existingPost) {
+			return res
+				.status(404)
+				.json({ success: false, message: 'Post unavailable' });
+		}
+		res
+			.status(200)
+			.json({ success: true, message: 'single post', data: existingPost });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.createPost = async (req, res) => {
+	const { title, description } = req.body;
+	const { userId } = req.user;
+	try {
+		const { error, value } = createPostSchema.validate({
+			title,
+			description,
+			userId,
+		});
+		if (error) {
+			return res
+				.status(401)
+				.json({ success: false, message: error.details[0].message });
+		}
+
+		const result = await Post.create({
+			title,
+			description,
+			userId,
+		});
+		res.status(201).json({ success: true, message: 'created', data: result });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.updatePost = async (req, res) => {
+    const {_id} = req.query;
+
+	const { title, description } = req.body;
+	const { userId } = req.user;
+	try {
+		const { error, value } = createPostSchema.validate({
+			title,
+			description,
+			userId,
+		});
+		if (error) {
+			return res
+				.status(401)
+				.json({ success: false, message: error.details[0].message });
+		}
+
+		const existingPost = await Post.findOne({_id});
+		if (!existingPost) {
+			return res
+				.status(404)
+				.json({ success: false, message: 'Post unavailable' });
+		}
+        if(existingPost.userId.toString() !== userId){
+            return res
+				.status(404)
+				.json({ success: false, message: 'Unauthorized' });
+        }
+        existingPost.title = title;
+        existingPost.description = description;
+
+        const result = await existingPost.save()
+        res.status(201).json({ success: true, message: 'updated', data: result });
+	
+    } catch (error) {
+		console.log(error);
+	}
+};
+
+
+exports.deletePost = async (req, res) => {
+    const {_id} = req.query;
+    
+	const { userId } = req.user;
+	try {
+
+		const existingPost = await Post.findOne({_id});
+		if (!existingPost) {
+			return res
+				.status(404)
+				.json({ success: false, message: 'Post already unavailable' });
+		}
+        if(existingPost.userId.toString() !== userId){
+            return res
+				.status(404)
+				.json({ success: false, message: 'Unauthorized' });
+        }
+       await Post.deleteOne({_id});
+        res.status(201).json({ success: true, message: 'deleted'});
+	
+    } catch (error) {
 		console.log(error);
 	}
 };
--- routers/postsRouters.js
+++ routers/postsRouters.js
@@ -5,10 +5,10 @@ const { indentifier } = require("../middlewares/identification");
 const router = express.Router()
 
 router.get('/all-posts', postsController.getPosts);
-// router.get('/signle-post', authController.signin);
-// router.post('/create-post', indentifier, authController.signout);
-// router.put('/update-post', indentifier, authController.changePasword);
-// router.delete('/delete-post', indentifier, authController.changePasword);
+router.get('/single-post', postsController.singlePost);
+router.post('/create-post', indentifier, postsController.createPost);
+router.put('/update-post', indentifier, postsController.updatePost);
+router.delete('/delete-post', indentifier, postsController.deletePost);
 
 
 
